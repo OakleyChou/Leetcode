6ac9769bf0a7f83d352e5683fb281f76c481d46b
OakleyChou <99305615+OakleyChou@users.noreply.github.com>
2022-08-04 10:18:03 +0800

    Update 013_Roman to Integer.py

--- python/013_Roman to Integer.py
+++ python/013_Roman to Integer.py
@@ -45,3 +45,21 @@ Constraints:
 s contains only the characters ('I', 'V', 'X', 'L', 'C', 'D', 'M').
 It is guaranteed that s is a valid roman numeral in the range [1, 3999].
 """
+
+class Solution():
+    def romanToInt(self,s):
+        dic={'I': 1, 'V': 5, 'X': 10, 'L': 50, 'C': 100, 'D': 500, 'M': 1000 }
+        x = 0
+        for i in range(len(s)-1):
+            if dic[s[i+1]] <= dic[s[i]]:
+                x += dic[s[i]]
+            else :
+                x -= dic[s[i]]
+        x += dic[s[-1]]
+        return x
+if __name__ == '__main__':
+    sol = Solution()
+    print( sol.romanToInt(s = "LVIII") ) #58
+    print( sol.romanToInt(s = "MCMXCIV") ) #1494
+
+

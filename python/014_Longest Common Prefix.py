d0f89d43b898903c224a5aa3861eaa6c663c28bb
OakleyChou <99305615+OakleyChou@users.noreply.github.com>
2022-08-10 14:24:31 +0800

    Update 014_Longest Common Prefix.py

--- python/014_Longest Common Prefix.py
+++ python/014_Longest Common Prefix.py
@@ -22,19 +22,29 @@ strs[i] consists of only lowercase English letters.
 
 class Solution():
     #從長的開始,若不相同就-1長度
+#    def longestCommonPrefix(self, strs):
+#        if len(strs) == 0:      #若strs是空的,返回''
+#            return ''
+#        if len(strs) == 1:      #若strs是單一字串,返回該字串
+#            return strs[0]
+#        max_len = len(strs[0])  #預設第一個字串長度為搜索長度
+#        for i in range(1,len(strs)):    
+#            max_len = min(max_len , len(strs[i])) 
+#            while max_len > 0 and strs[0][0: max_len] != strs[i][0:max_len]: #若目標跟新搜索字串不同,就將目標長度-1,直到找到最大公約數
+#                max_len -= 1
+#            if max_len == 0: #若最後目標長度=0,返回''
+#                return ''
+#        return strs[0][0: max_len]
+    
+    """用第一個當基準,看後續的開頭是不是等於目標word"""
+    #faster
     def longestCommonPrefix(self, strs):
-        if len(strs) == 0:      #若strs是空的,返回''
-            return ''
-        if len(strs) == 1:      #若strs是單一字串,返回該字串
-            return strs[0]
-        max_len = len(strs[0])  #預設第一個字串長度為搜索長度
-        for i in range(1,len(strs)):    
-            max_len = min(max_len , len(strs[i])) 
-            while max_len > 0 and strs[0][0: max_len] != strs[i][0:max_len]: #若目標跟新搜索字串不同,就將目標長度-1,直到找到最大公約數
-                max_len -= 1
-            if max_len == 0: #若最後目標長度=0,返回''
-                return ''
-        return strs[0][0: max_len]
+        word = strs[0]
+        for i in strs:
+            while len(word) != 0 and not i.startswith(word):
+                word = word[:-1]
+        return word
+
 
 
 
